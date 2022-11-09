14d5c09ed141f1a9dce1288de23a43246a3674b9
IceCreamCracker <matheus9bernard@hotmail.com>
2022-11-09 18:02:14 -0400

    main parsing finished

--- main.cpp
+++ main.cpp
@@ -14,12 +14,12 @@ struct Node {
     int* frequencies;
     Node* previous;
     Node* next;
-}
+};
 
 struct List {
     Node* first;
     Node* last;
-}
+};
 
     // Funções
 
@@ -34,7 +34,7 @@ List* createList() {
 void addElement(List* list, string word) {
     Node* element = new Node;
 
-    element->
+    //element->
 }
 
 string paraMaisBaixo (string str){ // to lower case
@@ -46,32 +46,35 @@ string paraMaisBaixo (string str){ // to lower case
         if (f > 0xff) f = int(c) - 0xffffff00; //UTF-8 em CHAR adiciona muitos bytes
         if(f >= 0x41 && f <= 0x5a || f >= 0x80 && f <= 0x9c) resultado += char(int(c) + 0x20);
         else resultado += c;
+    }
+
+    return resultado;
 }
 
 int main(int argc, char* argv[]){
     string line;
     ifstream myFile;
     string word;//gata, você é um editor de texto? pq vc é meu word 
-    //stringstream ss;
 
     for (int i = 1; i < argc; i++){
         myFile.open(argv[i]);
         if(myFile.is_open()){
             while (getline(myFile,line)){
-                  cout << line << endl;
-
+                  //cout << line << endl;
                   //separa a frase em palavras
-                  regex delimiters { "(\\,|\\;|\\:|\\.|\\?|\\!|\\s)+" };
+                  regex delimiters {"[\\,\\;\\:\\.\\?\\!\\s]+"};
                   sregex_token_iterator tokens_begin { line.begin(), line.end(), delimiters, -1 };
                   auto tokens_end = sregex_token_iterator {};
                   for (auto token_it = tokens_begin; token_it != tokens_end; token_it++)
-                        cout << "* [" << *token_it << "]\n";
-                
+                        cout << paraMaisBaixo(*token_it) << endl; // funcionando até aqui
             } 
             myFile.close();
         } else {
             cout << "Entrada inválida!";
             return -1;
-        } 
+        }
     }
+
+
+    return 0;
 }
